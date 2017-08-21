855865c2f904eca3104b464259a611a74a1192e7
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-08-21 16:07:04 +0100

    add auth views

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 feature "User" do
   let(:email) {'user@gmail.com'}
-  let(:password) {'123'}
-  
+  let(:password) {'123456abc.'}
+
   scenario "I can sign up with new email and password" do
-    visit('/user/new')
-    fill_in 'email', with: email
-    fill_in 'password', with: password
+    visit('/users/sign_up')
+    fill_in 'user_email', with: email
+    fill_in 'user_password', with: password
+    fill_in 'user_password_confirmation', with: password
     click_button 'Sign up'
     expect(page).to have_content('Hello, user@gmail.com')
   end
