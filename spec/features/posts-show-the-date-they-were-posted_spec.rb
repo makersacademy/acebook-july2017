57a94506874c2cdf2cad805615cd64e7f6ed2543
Toby Hamand <tobyhamand@gmail.com>
2017-08-21 15:26:36 +0100

    added tests and reworded css classes

--- spec/features/posts-show-the-date-they-were-posted_spec.rb
+++ spec/features/posts-show-the-date-they-were-posted_spec.rb
@@ -1,22 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Date Posted", type: :feature do
-  before do
-    time = Time.now.utc
-    Timecop.freeze(time)
-  end
-
-  after do
-    Timecop.return
-  end
-
   scenario "Newly created posts show the date they were posted" do
-    allow(Time).to receive(:now).and_return(Time.now.utc)
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
-    timenow = Time.now
     click_button "Submit"
-    expect(page).to have_content(timenow.strftime("%H:%M (%d/%m/%y)"))
+    post = Post.find_by(message: "Hello, world!")
+    expect(page).to have_content(post.created_at.strftime("%H:%M (%d/%m/%y)"))
   end
 end
