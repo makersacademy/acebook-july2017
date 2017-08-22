c27a073e85a2809932a1edf57918c9442516b76e
Sapphire Mason-Brown <saphmb@gmail.com>
2017-08-22 16:18:08 +0100

    Tests for creation of likes via controller

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -4,22 +4,11 @@ RSpec.describe LikesController, type: :controller do
 
 
   describe "POST /" do
-    # it "responds with 200" do
-    #   post :create, params: { like: { post_id: 1 } }
-    #   expect(response).to redirect_to(posts_url)
-    # end
 
-		let(:post) {create(:post, message: "A post")}
+		let!(:post_mock) {create(:post, message: "A post", id: 1)}
     it "creates a like" do
-			# likes = Like.all
-			# Post.likes.build()
 
-			like = Like.new(post_id: post.id, created_at: "2017-08-22 11:46:38.097751", updated_at: "2017-08-22 11:46:38.097751")
-			p like.validate
-			p like.errors
-			p post_mock
-			# post :create, params: { like: { post_id: 1 } }
-			p Like.all
+			post :create, params: { like: { post_id: 1 } }
       expect(Like.find_by(post_id: 1 )).to be
     end
   end
