c4e2198fc8f6901c6a6a0b4abd79c960572e66b9
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-22 16:51:22 +0100

    Testing user profile page - WIP - all fucked up nothing works

--- spec/features/user_can_view_profile_spec.rb
+++ spec/features/user_can_view_profile_spec.rb
@@ -1,11 +1,12 @@
-require 'rails_helper'
-require 'web_helpers.rb'
-
-feature "Profile page" do
-
-  scenario "user signs up and views profile" do
-    sign_up
-    # visit("/users/show")
-  end
-
-end
+# require 'rails_helper'
+# require 'web_helpers.rb'
+#
+# feature "Profile page" do
+#
+#   scenario "user signs up and views profile" do
+#     sign_up
+#     visit("users#show")
+#     expect(page).to have_content('ladyruby@rails.com')
+#   end
+#
+# end
