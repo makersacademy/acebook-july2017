cfb1226eaf6693b55a2844e6307c6f767360f4db
Edward Withers <dearshrewdwit@gmail.com>
2017-08-22 12:26:20 +0100

    devise_update

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,6 @@ ActiveRecord::Schema.define(version: 20170821143343) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 50
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -4,12 +4,25 @@ feature "User" do
   let(:email) {'user@gmail.com'}
   let(:password) {'123456abc.'}
 
-  scenario "I can sign up with new email and password" do
+  scenario "I can sign up" do
     visit('/users/sign_up')
     fill_in 'user_email', with: email
     fill_in 'user_password', with: password
     fill_in 'user_password_confirmation', with: password
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+
+  context "sign in" do
+    let!(:user) { User.create(email: "test@test.com", password: "test12") }
+
+    scenario "I can sign in" do
+      visit("/users/sign_in")
+      fill_in 'user_email', with: "test@test.com"
+      fill_in 'user_password', with: "test12"
+      click_button 'Log in'
+      click_link 'Sign out'
+      expect(current_path).to eq "/users/sign_in"
+    end
   end
 end
