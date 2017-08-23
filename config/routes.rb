01c4051c4f1b9aa5cab292e0598fd60f94227bc4
annalaise <annalaise.gibbons@gmail.com>
2017-08-23 15:20:28 +0100

    removed redundant test

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'posts'
   devise_for :users
-  resources :users do
-    resources :posts
-  end 
+  resources :posts
+  resources :users 
   root to: 'posts#index'
 
 end
