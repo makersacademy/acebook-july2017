c0f287aacccef0de90eac987d9b456f8f100b7b0
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-24 10:35:02 +0100

    Clean up album test

--- db/schema.rb
+++ db/schema.rb
@@ -63,9 +63,6 @@ ActiveRecord::Schema.define(version: 20170823160857) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/features/albums_can_be_created_spec.rb
+++ spec/features/albums_can_be_created_spec.rb
@@ -5,8 +5,6 @@ feature 'Album' do
   let(:test_album_name) { 'My days at Makers' }
 
   before do
-    p "Album test"
-
     visit("/users/sign_in")
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
