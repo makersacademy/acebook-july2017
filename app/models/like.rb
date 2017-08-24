6a219a64ae09e7cdc87ed53fd087fb3ecc2bbe02
Alison Woodman <aliwoodman@gmail.com>
2017-08-24 15:25:48 +0100

    Test for newsfeed showing like activity

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
+  include PublicActivity::Model
+  tracked
+
   belongs_to :post
   belongs_to :user
   validates :user, uniqueness: {scope: :post_id}
--- spec/features/page-does-not-change-when-button-is-clicked_spec.rb
+++ spec/features/page-does-not-change-when-button-is-clicked_spec.rb
@@ -6,8 +6,10 @@ RSpec.feature "Like", type: :feature do
   before do
     login(user)
     visit "/posts"
-    fill_in "post_message", with: "Hello, world!"
-    click_button "Submit"
+    within ".new_post" do
+      fill_in "post_message", with: "Hello, world!"
+      click_button "Submit"
+    end
   end
 
   scenario "page does not change when button like is clicked" do
