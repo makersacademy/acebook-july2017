d23203c12a964a3d1d2b34494ce256a0fc638b75
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-24 16:27:48 +0100

    Add Amazon S3 integration - no TDD yet

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :album
 
-  has_attached_file :image
+  has_attached_file :data
 
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :data, :content_type => /\Aimage\/.*\Z/
 end
