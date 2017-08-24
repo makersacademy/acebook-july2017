0785ee367ae7ef06a40870f5470c184bb537d374
Toby Hamand <tobyhamand@gmail.com>
2017-08-24 12:13:05 +0100

    created migration for removing null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823152351) do
+ActiveRecord::Schema.define(version: 20170824111217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,9 @@ ActiveRecord::Schema.define(version: 20170823152351) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -57,10 +59,6 @@ ActiveRecord::Schema.define(version: 20170823152351) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "students", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 50 
   end
 
   create_table "users", force: :cascade do |t|
@@ -87,6 +85,7 @@ ActiveRecord::Schema.define(version: 20170823152351) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
