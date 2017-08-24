bb5f46e63b1193b2ca8bc612a967d805c9fb9e2c
chuk-chuk <yulia.v.litvin@gmail.com>
2017-08-24 12:19:13 +0100

    amend test for user profile and attempted to save edited user info to DB, still to be implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+# trying to update user profile info and save it to DB = TO BE IMPLEMENTED
+
+  # def edit
+  #   @user = User.find(params[:id])
+  #   @user.update_attributes(params[:user])
+  #   @user.changed?
+  #
+  # end
 
 end
