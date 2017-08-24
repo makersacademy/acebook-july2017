6d12946ae9f0e44751ae889e5859e720d2f39dd5
Alison Woodman <aliwoodman@gmail.com>
2017-08-24 15:25:48 +0100

    Test for newsfeed showing like activity

--- spec/features/page-does-not-change-when-button-is-clicked_spec.rb
+++ spec/features/page-does-not-change-when-button-is-clicked_spec.rb
@@ -11,8 +11,10 @@ RSpec.feature "Like", type: :feature do
       click_button 'Log in'
     end
     visit "/posts"
-    fill_in "post_message", with: "Hello, world!"
-    click_button "Submit"
+    within ".new_post" do
+      fill_in "post_message", with: "Hello, world!"
+      click_button "Submit"
+    end
   end
 
   scenario "page does not change when button like is clicked" do
