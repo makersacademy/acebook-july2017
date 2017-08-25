aa067354a7b114c1daae6be337a7862733d7308d
artemis <artemisxen@gmail.com>
2017-08-25 16:57:37 +0100

    added link to posts from profile page

---
+++ spec/features/link_to_posts_from_profile_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "Add link to posts" do
+  let!(:user) { create(:user) }
+
+  before do
+    login(user)
+  end
+
+  scenario "profile page has a link to posts" do
+    visit user_path(user)
+    click_link "Show posts"
+    expect(current_path).to eq("/posts")
+  end
+end
