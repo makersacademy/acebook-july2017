36ebb1bce8d618cdea9ab8206f4ad8b49ea352d1
Alexander-Blair <zanderb@hotmail.co.uk>
2017-08-25 10:13:09 +0100

    Images shown, OK to hand over

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ImagesController, type: :controller do
-
+  before do
+    allow_any_instance_of(Paperclip::Attachment).receive(:save).and_return(true)
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
+  before do
+    allow_any_instance_of(Paperclip::Attachment).receive(:save).and_return(true)
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
