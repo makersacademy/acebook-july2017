44783734281239a4b157b97d15cd917fe5468e76
Toby Hamand <tobyhamand@gmail.com>
2017-08-25 14:30:14 +0100

    added comments to news_feed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  MAX_SUMMARY_LENGTH = 20
+
+  include PublicActivity::Model
+  tracked
+
+  def summary
+    return message unless message.length > MAX_SUMMARY_LENGTH
+    message[0...MAX_SUMMARY_LENGTH] + "..."
+  end
 end
